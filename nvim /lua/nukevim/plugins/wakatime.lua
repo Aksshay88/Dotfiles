943790ab049a67a2a8ca5dc43456ab019ac9c32c
Aksshay88 <119944779+Aksshay88@users.noreply.github.com>
2024-07-16 18:52:40 +0530

    Secret key disabled

--- nvim /lua/nukevim/plugins/wakatime.lua
+++ nvim /lua/nukevim/plugins/wakatime.lua
@@ -2,4 +2,4 @@ require('packer').startup(function()
   use 'wakatime/vim-wakatime'
 end)
 
-vim.g.wakatime_api_key = 'waka_a894d8cc-0029-48cb-a5cb-1ab57ac06884'
+vim.g.wakatime_api_key = 'your_api_key'
